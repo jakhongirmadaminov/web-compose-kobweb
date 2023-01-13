abdda87dcd913617049fc7d708d53c5509317b2e
Jakhongir Madaminov <Jakhongir.Madaminov@cantire.com>
2023-01-13 22:56:35 +0500

    Initial commit

---
+++ site/src/jsMain/kotlin/org/example/smartposweb/components/widgets/GoHomeLink.kt
@@ -0,0 +1,7 @@
+package org.example.smartposweb.components.widgets
+
+import androidx.compose.runtime.Composable
+import com.varabyte.kobweb.silk.components.navigation.Link
+
+@Composable
+fun GoHomeLink() = Link("/", "Go Home")
